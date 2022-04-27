9f6d6fa0aa80f0b8d01dc46604c6117d4ba6a952
santiago <danielsantiago1230@gmail.com>
2022-04-27 01:08:45 -0300

    avance

--- ejercicios.js
+++ ejercicios.js
@@ -1,1 +1,16 @@
-alert("hola mundo")
+//Me parecio emocionante resolver los problemas con un entorno visual, por ende cree el siguiente codigo
+//la función handleSubmit recibe atravez del evento la paralabra ingresada por el usuario, y  resolveremos el ejercicio 1
+
+
+const handleChange=(event)=>{
+    // arrayPalabra será un arreglo de caracteres de la palabra
+    const [...arrayPalabra]=event.target.value
+
+
+}
+
+const handleSubmit=(event)=>{//
+    //corta con la naturaleza del submit que es recargar la pagina
+    event.preventDefault();
+}
+
